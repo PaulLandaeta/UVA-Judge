255d85a36679c666e9759d40d7d5960219ba6930
paul-landata-tierconnect-com <paul.landaeta@tierconnect.com>
2015-10-07 10:25:46 -0400

    in circle cuidado caso 0 0 0

---
+++ 10195/10195.cpp
@@ -0,0 +1,48 @@
+/* go go Accepted*/
+#include <bits/stdc++.h>
+#define input freopen("in.txt","r",stdin)
+#define output freopen("out.txt","w",stdout)
+using namespace std;
+#define EPS 1e-9
+#define PI acos(-1.0)
+
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+struct point { double x, y;   // only used if more precision is needed
+  point() { x = y = 0.0; }                      // default constructor
+  point(double _x, double _y) : x(_x), y(_y) {} };      // constructor
+
+double dist(point p1, point p2) {
+  return hypot(p1.x - p2.x, p1.y - p2.y); }
+
+double perimeter(double ab, double bc, double ca) {
+  return ab + bc + ca; }
+
+double perimeter(point a, point b, point c) {
+  return dist(a, b) + dist(b, c) + dist(c, a); }
+
+double area(double ab, double bc, double ca) {
+  // Heron's formula, split sqrt(a * b) into sqrt(a) * sqrt(b); in implementation
+  double s = 0.5 * perimeter(ab, bc, ca);
+  return sqrt(s) * sqrt(s - ab) * sqrt(s - bc) * sqrt(s - ca); }
+
+double area(point a, point b, point c) {
+  return area(dist(a, b), dist(b, c), dist(c, a)); }
+  double rCircumCircle(double ab, double bc, double ca) {
+  return ab * bc * ca / (4.0 * area(ab, bc, ca)); }
+  double rInCircle(double ab, double bc, double ca) {
+  return area(ab, bc, ca) / (0.5 * perimeter(ab, bc, ca)); }
+int main() {
+	//input;
+	double a,b,c;
+	while(scanf("%lf %lf %lf",&a,&b,&c)==3){
+    if(a+b+c==0)
+		printf("The radius of the round table is: %.3lf\n",0.0);
+  else
+    printf("The radius of the round table is: %.3lf\n",rInCircle(a,b,c));
+	}
+
+	return 0;
+}
