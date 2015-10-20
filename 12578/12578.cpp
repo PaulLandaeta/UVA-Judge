335a64a4578b895b16ab0aa2d130579c5302813a
paul-landata-tierconnect-com <paul.landaeta@tierconnect.com>
2015-10-20 14:54:09 -0400

    regla de tres y areas

---
+++ 12578/12578.cpp
@@ -0,0 +1,22 @@
+/* go go Accepted*/
+#include <bits/stdc++.h>
+#define input freopen("in.txt","r",stdin)
+#define output freopen("out.txt","w",stdout)
+using namespace std;
+#define EPS 1e-9
+#define PI acos(-1.0)
+
+int main() {
+	input;
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		double r=n/5.0;
+		double h=(6*n)/10.0;
+		printf("%.2f %.2f\n",r*r*PI,h*n-r*r*PI);
+
+	}
+	return 0;
+}
